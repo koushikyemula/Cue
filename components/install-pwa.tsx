f42d15c5d0fd2e846149c903bf3d2064b99d7cc3
Koushik <koushikyemula@gmail.com>
2025-05-09 11:43:46 +0530

    chore: simplified instal-pwa component

--- components/install-pwa.tsx
+++ components/install-pwa.tsx
@@ -9,15 +9,6 @@ interface BeforeInstallPromptEvent extends Event {
 
 export function InstallPWA({ promptDelay = 1500 }: { promptDelay?: number }) {
   useEffect(() => {
-    const isMobile =
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      );
-
-    if (!isMobile) return;
-
-    if (localStorage.getItem("pwa-prompt-dismissed") === "true") return;
-
     const isStandalone =
       window.matchMedia("(display-mode: standalone)").matches ||
       (navigator as any).standalone === true;
@@ -28,15 +19,7 @@ export function InstallPWA({ promptDelay = 1500 }: { promptDelay?: number }) {
       e.preventDefault();
 
       setTimeout(() => {
-        e.prompt()
-          .then(() => {
-            return e.userChoice;
-          })
-          .then((choiceResult) => {
-            if (choiceResult.outcome === "dismissed") {
-              localStorage.setItem("pwa-prompt-dismissed", "true");
-            }
-          });
+        e.prompt();
       }, promptDelay);
     };
 
